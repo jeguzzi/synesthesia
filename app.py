bf263e280ba9c79b3bfd6dd5d4e503358b57aeba
Jérôme Guzzi <jerome@idsia.ch>
2015-12-17 10:13:32 +0100

    Added set_led_list to set multiple color at once

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from sse import MyServerSentEvent as SSE
 from gevent.queue import Queue
 import zmq.green as zmq
 import gevent
-
+import logging
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('main.html')
 
 
@@ -28,10 +28,14 @@ def stream_led():
             try:
                 (led, color) = q.get()
             except Exception, e:
-                print e
+                logging.error(e)
                 break
-            (r, g, b) = color
-            svg_color = 'rgb({r},{g},{b})'.format(r=r, g=g, b=b)
+            # Send a list of colors
+            svg_color = ['rgb({r},{g},{b})'.format(r=r, g=g, b=b)
+                         for (r, g, b) in color]
+            # Send a single color
+            # (r, g, b) = color
+            # svg_color = 'rgb({r},{g},{b})'.format(r=r, g=g, b=b)
             ev = SSE(svg_color)
             ev.id = led + 1
             yield ev.encode()
@@ -57,7 +61,7 @@ def listen():
     context = zmq.Context()
     socket = context.socket(zmq.PULL)
     socket.bind("tcp://*:%d" % port)
-    print 'listening...'
+    # print 'listening...'
     while True:
         led, color = socket.recv_json()
         set_led(led, color)
--- sim_led.py
+++ sim_led.py
@@ -9,4 +9,9 @@ sock.connect("tcp://localhost:%d" % port)
 
 def set_led(led, rgb):
     """Set the led with id <led> to an rgb (r,g,b)"""
-    sock.send_json((led, rgb))
+    sock.send_json((led, [rgb]))
+
+
+def set_led_list(led, rgbs):
+    """Set the led starting with id <led> to an rgb list [(r,g,b)]"""
+    sock.send_json((led, rgbs))
